7b0fa437510a9bb1b6881f69e187789ddf84184c
HyunJuneKang <halfkang67@naver.com>
2026-06-22 17:28:14 +0900

    [level 1] Title: K번째수, Time: 1.87 ms, Memory: 74.4 MB -BaekjoonHub

--- 강현준/프로그래머스/1/42748. K번째수/K번째수.java
+++ 강현준/프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,26 +1,35 @@
+/*
+array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3
+array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다. arr만들기 i = 2;i < j+1 ;i++
+1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.   Arrays.sort()
+2에서 나온 배열의 3번째 숫자는 5입니다.    int num = arr[n-1]
+*/
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        
-        int N = commands.length;
-        int [] arrAns = new int[N];
-        for (int i = 0; i < N ; i++){
+        ArrayList<Integer>result = new ArrayList<>();
+        for ( int i = 0 ; i < commands.length;i++){
             int startPos = commands[i][0]-1;
             int endPos = commands[i][1]-1;
-            int K = commands[i][2];
-            int [] arrDivide = new int[endPos-startPos+1];
-            
-            int j = 0;
-            for (int k = startPos; k <= endPos;k++){
-                arrDivide[j] = array[k];
-                j++;
+            int targetPos = commands[i][2];
+            //자르기
+            int []newArr = new int[endPos-startPos+1];
+            int index = 0;
+            for (int j = startPos; j < endPos+1;j++){
+                newArr[index++] = array[j];
             }
-
-            Arrays.sort(arrDivide);
-            arrAns[i] = arrDivide[K-1];
-            
+            // 정렬
+            Arrays.sort(newArr);
+            // target 찾기
+            int targetNum = newArr[targetPos-1];
+            // 결과 저장
+            result.add(targetNum);
         }
-        return arrAns;
+        
+        int[] answer = new int[result.size()];
+        for (int i = 0 ; i < result.size(); i++){
+            answer[i] = result.get(i);
+        }
+        return answer;
     }
 }
