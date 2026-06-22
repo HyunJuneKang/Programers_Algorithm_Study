5d215567d8feabf0c2b8ccda331b754f08abf586
HyunJuneKang <halfkang67@naver.com>
2026-06-22 17:16:43 +0900

    [level 0] Title: 특이한 정렬, Time: 0.53 ms, Memory: 74.7 MB -BaekjoonHub

--- 강현준/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 강현준/프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -1,22 +1,28 @@
+/*
+sort기준을 n을 기준으로 해서 sort해야함
+return abs(a-n) - abs (b-n)
+abs(a-n) 이 abs(b-n) 이랑 같으면
+return b-a
+*/
 import java.util.*;
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        Integer[] numArr = new Integer[numlist.length];
-        for(int i = 0; i < numlist.length;i++){
-            numArr[i] = numlist[i];
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0 ; i < numlist.length; i++){
+            arr.add(numlist[i]);
         }
         
-        Arrays.sort(numArr, (a, b) -> {
-            int diff = Math.abs(a - n) - Math.abs(b - n);
-            if (diff == 0 ){
-                return b-a;
-            } 
-            return diff;
-        }); 
-        
-        int[] answer = new int[numArr.length];
-        for(int i = 0; i < numlist.length;i++){
-            answer[i] = numArr[i];
+        Collections.sort(arr,new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a , Integer b) {
+                if (Math.abs(a-n) == Math.abs(b-n))
+                    return b-a;
+                return Math.abs(a-n) - Math.abs(b-n);
+            }
+        });
+        int [] answer = new int[arr.size()];
+        for (int i = 0 ; i < arr.size();i++){
+            answer[i] = arr.get(i);
         }
         return answer;
     }
