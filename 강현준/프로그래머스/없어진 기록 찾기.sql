6ea5a1fd5f0b208bb8c6d8035181944449b73ed6
HyunJuneKang <halfkang67@naver.com>
2026-06-25 18:30:50 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 강현준/프로그래머스/없어진 기록 찾기.sql
@@ -0,0 +1,9 @@
+-- 천재지변으로 인해 일부 데이터가 유실되었습니다. 
+-- 입양을 간 기록은 있는데, 
+-- 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+SELECT i.ANIMAL_ID , i.NAME
+        FROM ANIMAL_OUTS i
+        LEFT OUTER JOIN ANIMAL_INS o 
+        ON i.ANIMAL_ID = o.ANIMAL_ID
+        WHERE o.ANIMAL_ID IS NULL
+        ORDER BY ANIMAL_ID,NAME;
